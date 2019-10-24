f10cbfa878d985f3bec8dceff0680108013848f7
Chris Mastel <chris.mastel@gmail.com>
2019-10-24 19:32:17 +0000

    separated into two files

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,15 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const searchTerm = args[0];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${searchTerm}`, (error, response, body) => {
-  if (error) {
-    console.log('An error occured. Please try again.');
-    process.exit();
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      process.exit();
+    }
 
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log('Sorry, no breed found that matches that search term.');
-    process.exit();
-  }
-  console.log(data[0].description);
+    callback(error, data[0].description);
+  });
+  
+};
 
-});
-
+module.exports = { fetchBreedDescription };
